619b6801e3ee796e94028d95724dab32df320b9f
Pooja Pathak <poojapathak0001@gmail.com>
2018-02-27 16:52:37 +0530

    Updated index.jsp and index.js

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,52 +1,44 @@
 
 	function getData(){
       var xmlHttp = new XMLHttpRequest();
-      var movie= document.getElementById("moviename").value;
+      var word= document.getElementById("usertext").value;
+      alert(word);
 
-      //Movie search api from tmdb
-      var url = "https://api.themoviedb.org/3/search/movie?api_key=628feb1e5d19aa715bd6f0824546c81d&query=" + movie;
+      
+      var url = "https://api.themoviedb.org/3/search/movie?api_key=628feb1e5d19aa715bd6f0824546c81d&query="+word;
 
       xmlHttp.onreadystatechange = function() {
+    	  if(this.status == 404) {
+    		  document.getElementById('titlemovie1').innerHTML = "404<br><h1>Movie Not Found</h1>";
+    	  }
           if(this.readyState == 4 && this.status == 200){
               var myArr= JSON.parse(this.responseText);
-              var dataObj= myArr;
-             
-              //searched result item 1 
-              document.getElementById('titlemovie1').innerHTML = "Title = " +
-             				dataObj.results[0].title ;
-             document.getElementById('movie1').innerHTML = "<br> Ratings =" + dataObj.results[0].vote_average + 
-             				"<br> Release Date =" + dataObj.results[0].release_date + "<br> Overviews =" + 
-             				dataObj.results[0].overview ;
-             //searched result item 2
-             document.getElementById('titlemovie2').innerHTML = "Title = " + 
-             				dataObj.results[1].title ;
-             document.getElementById('movie2').innerHTML = "<br> Ratings =" + dataObj.results[1].vote_average + 
-             				"<br> Release Date =" + dataObj.results[1].release_date + "<br> Overviews =" + 
-             				dataObj.results[1].overview;
-             //searched result item 3
-             document.getElementById('titlemovie3').innerHTML = "Title = " +
-							dataObj.results[2].title;
-             document.getElementById('movie3').innerHTML = "<br> Ratings =" + dataObj.results[2].vote_average + 
-							"<br> Release Date =" + dataObj.results[2].release_date + "<br> Overviews =" + 
-							dataObj.results[2].overview ;
-			//searched result item 4
-			document.getElementById('titlemovie4').innerHTML = "Title = " + 
-							dataObj.results[3].title;
-			document.getElementById('movie4').innerHTML = "<br> Ratings =" + dataObj.results[3].vote_average + 
-							"<br> Release Date =" + dataObj.results[3].release_date + "<br> Overviews =" + 
-							dataObj.results[3].overview;
-			//searched result item 5
-			document.getElementById('titlemovie5').innerHTML = "Title = " + 
-							dataObj.results[4].title;
-			document.getElementById('movie5').innerHTML = "<br> Ratings =" + dataObj.results[4].vote_average + 
-							"<br> Release Date =" + dataObj.results[4].release_date + "<br> Overviews =" + 
-							dataObj.results[4].overview;
-				
-	}   
+              var data= myArr;
+              var htmlText = '';
+              for ( var i=0;i<data.results.length;i++ ) {
+                  console.log(data.results[i].title);
+                  htmlText += '<div class="div-conatiner">';
+                  htmlText += '<div class="col_sm_4">';
+                  htmlText += '<p class="p-name" id="i"> Name: ' + data.results[i].title + '</p>';
+                  htmlText += '<p class="p-loc">Poster: <img src="http://image.tmdb.org/t/p/w500' + data.results[i].poster_path + '" width=200 height=300></p>';
+                  htmlText += '<p class="p-desc"> Description: ' + data.results[i].overview + '</p>';
+                  htmlText += '<p class="p-created"> Released on: ' + data.results[i].release_date + '</p>';
+                  htmlText += '</div>';
+                  htmlText += '<button onClick="addFav()" >Add to favourites</button>';
+                  htmlText += '<script type="text/javascript" src="MovieServlet.java"></script>';
+                  htmlText += '<input type="button" value="Remove Favourite" />';
+                  htmlText += '<hr>';
+              }
+           document.getElementById('titlemovie1').insertAdjacentHTML('afterend',htmlText);     
+          }
       };
 
       xmlHttp.open("GET",url,true);
       xmlHttp.send();
 
     }
-
+	
+	function addFav() {
+		var xmlHttp = new XMLHttpRequest();
+		
+	}
